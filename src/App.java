64619227dee92e2899784066e741d14389890dc4
Marcus Moore <macum98@gmail.com>
2026-09-14 18:54:21 -0700

    Made Grid
    10x10 Grid now draws

--- src/App.java
+++ src/App.java
@@ -5,14 +5,21 @@ public class App extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.BLACK);
-        int offsetx = 80;
-        int offsety = 80;
-        for (int c = 0; c < 10; c++){
-            g.drawLine(offsetx, offsety, offsetx, offsety + );
-            offsetx += 20;
-            offsety += 20;
+        Graphics2D g2d = (Graphics2D) g;
+        super.paintComponent(g2d);
+        g2d.setColor(Color.BLACK);
+        g2d.setStroke(new BasicStroke(2, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        int offset = 40;
+        int offsetx = offset;
+        int offsety = offset;
+        for (int c = 0; c < 11; c++){
+            g2d.drawLine(offsetx, offsety, offsetx, offsety + 320);
+            offsetx += 32;
+        }
+        offsetx = offset;
+        for (int r = 0; r < 11; r++){
+            g2d.drawLine(offsetx, offsety, offsetx + 320, offsety);
+            offsety += 32;
         }
         //g.setColor(Color.BLUE);
         //g.fillRect(50, 50, 200, 100);
